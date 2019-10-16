3c757152d82db4c34457702c597f4839a35c09d3
Ty Conway <wtyconway@gmail.com>
2019-10-16 12:37:11 -0500

    Add initial files

---
+++ PriorityQueue.h
@@ -0,0 +1,42 @@
+#ifndef PRIORITY_QUEUE_H
+#define PRIORITY_QUEUE_H
+
+#include <exception>
+
+using namespace std;
+
+/**
+ * Note: in order to try to make things easier, the queue below will only have a single type of value
+ * in it instead of a (key, value) pair.  You could still store a (key, value) pair, but you would
+ * make a priority queue that stored an object with both the key/value in it and a comparison operator
+ * that only compared the keys.
+ */
+
+template <class Type>
+class PriorityQueue
+{
+private:
+	// data here
+
+public:
+   PriorityQueue(void);
+
+   ~PriorityQueue(void);
+
+   bool isEmpty(void);
+
+   int size(void);
+
+   // inserts a piece of data into the priority queue
+   void insertItem ( Type data );
+
+   // removes and returns the minimum value in the queue
+   // throws an exception if the queue is empty
+   Type removeMin ( void );
+
+   // return the minimum value in the queue without removing it
+   // throws an exception if the queue is empty
+   Type minValue ( void );
+};
+
+#endif
