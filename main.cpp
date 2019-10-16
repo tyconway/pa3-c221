59fa39e66bf21db0bea4ae3c818006067498415e
Ty Conway <wtyconway@gmail.com>
2019-10-16 13:25:58 -0500

    Add method to parse file to vector format

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
 #include "PriorityQueue.h"
 
+vector<int> parseFile(string filename) {
+    ifstream fin;
+    fin.open(filename);
+    int number;
+    vector<int> vector;
+    while (fin >> number) {
+        vector.push_back(number);
+    }
+    return vector;
+}
+
 int main() {
-    
+    string file = "numbers.txt";
+    vector<int> data = parseFile(file);
 }
